db8b73cf8429eb6c8f88e4eb87c70f6ce3bc4333
Tai <gwckoeda@gmail.com>
2024-04-27 03:23:03 -0400

    amazon search with asin and no asin

--- christians-fragment/SQL-DATABASE/SQL-DATABASE/Controllers/ScanController.cs
+++ christians-fragment/SQL-DATABASE/SQL-DATABASE/Controllers/ScanController.cs
@@ -10,9 +10,11 @@ namespace SQL_DATABASE.Controllers
     [Route("[controller]")]
     public class ScanController : ControllerBase
     {
+        // api key
+        private const string APIKEY = "";
         private static HttpClient client = new HttpClient();
-        private const string baseUrl = "https://world.openpetfoodfacts.org/api/v2/product/";
 
+        // Used when ASIN is not available
         public class Offer
         {
             public string ReviewRating { get; set; }
@@ -20,6 +22,33 @@ namespace SQL_DATABASE.Controllers
             public string Price { get; set; }
             public string Link { get; set; }
             public string Title { get; set; }
+        }
+
+        // Used when ASIN is available
+        public class SellerOffer
+        {
+            public string ProductPrice { get; set; }
+            public string ProductCondition { get; set; }
+            public string ShipsFrom { get; set; }
+            public string Seller { get; set; }
+            public string SellerLink { get; set; }
+            public string SellerStarRating { get; set; }
+            public string SellerStarRatingInfo { get; set; }
+            public string Currency { get; set; }
+            public string DeliveryPrice { get; set; }
+            public string DeliveryTime { get; set; }
+        }
+
+        // Used to store product details
+        public class ProductDetails
+        {
+            public string Name { get; set; }
+            public string Ingredients { get; set; }
+            public string Asin { get; set; }
+            //  when ASIN is not available this is used
+            public List<Offer> AmazonOffers { get; set; }
+            // when ASIN is available this is used 
+            public List<SellerOffer> AmazonSellerOffers { get; set; }
         }
 
         public class BarcodeInput
@@ -33,23 +62,36 @@ namespace SQL_DATABASE.Controllers
             if (string.IsNullOrEmpty(input.Barcode))
                 return BadRequest("No barcode provided.");
 
-            // Get product info from openpetfoodfacts API
-            string productInfo = await GetProductInfo(input.Barcode);
-            if (string.IsNullOrEmpty(productInfo))
+            // Get product info from openpetfoodfacts, upcitemdb, and big-product-data API
+            ProductDetails productInfo = await FetchProductDetails(input.Barcode);
+
+            /*Console.WriteLine("Product Name: " + productInfo.Name);
+            Console.WriteLine("Product Ingredients: " + productInfo.Ingredients);
+            Console.WriteLine("Product ASIN (AMAZON): " + productInfo.Asin);*/
+
+            if (string.IsNullOrEmpty(productInfo.Name))
                 return NotFound("Product information not found.");
 
             try
             {
-                var parsedProductInfo = JObject.Parse(productInfo);
-                var name = parsedProductInfo["product"]["product_name"]?.ToString();
-                if (!string.IsNullOrEmpty(name))
+                //if productinfo.asin is not null, search for the product on amazon using the asin
+                if (!string.IsNullOrEmpty(productInfo.Asin))
                 {
-                    string apiKey = ""; // Replace with your actual API key
-                    List<Offer> amazonResults = await AmazonSearchResult(name, apiKey);
-                    return Ok(amazonResults);
+                    List<SellerOffer> amazonResults = await AmazonSearchResultASIN(productInfo.Asin);
+                    productInfo.AmazonSellerOffers = amazonResults;
+                    Console.WriteLine("Amazon Results from ASIN: " + amazonResults);
+                }   
+                else if (!string.IsNullOrEmpty(productInfo.Name))
+                {
+                    List<Offer> amazonResults = await AmazonSearchResult(productInfo.Name);
+                    productInfo.AmazonOffers = amazonResults;
+                } 
+                else
+                {
+                    return NotFound("Product name not found in product info.");
                 }
 
-                return NotFound("Product name not found in product info.");
+                return Ok(productInfo);
             }
             catch (Exception ex)
             {
@@ -57,29 +99,106 @@ namespace SQL_DATABASE.Controllers
             }
         }
 
-        private static async Task<string> GetProductInfo(string barcode)
+        // Fetch product details from openpetfoodfacts, upcitemdb, and big-product-data APIs
+        public static async Task<ProductDetails> FetchProductDetails(string barcode)
         {
-            string url = baseUrl + barcode;
-            using (var response = await client.GetAsync(url))
+            ProductDetails details = new ProductDetails();
+
+            // Fetch product name and basic info
+            string openPetFoodFactUrl = "https://world.openpetfoodfacts.org/api/v2/product/" + barcode; // baseUrl should be adjusted as necessary
+            var productInfoResponse = await client.GetAsync(openPetFoodFactUrl);
+            if (productInfoResponse.IsSuccessStatusCode)
             {
-                if (response.IsSuccessStatusCode)
+                string productInfo = await productInfoResponse.Content.ReadAsStringAsync();
+                var productJson = JObject.Parse(productInfo);
+                Console.WriteLine("openpetfoodfacts:" + productJson);
+                details.Name = productJson["product"]["product_name"]?.ToString() ?? null;
+                /*if (details.Name == null)
+                    Console.WriteLine("Product name not found in openpetfoodfacts API.");*/
+            }
+            else
+            {
+                return null;  // Or throw an exception or handle the error as required
+            }
+
+            // Fetch ASIN from another API
+            string upcItemDBUrl = $"https://api.upcitemdb.com/prod/trial/lookup?upc={barcode}";
+            client.DefaultRequestHeaders.Clear();  // Clear previous headers
+            var upcItemDBResponse = await client.GetAsync(upcItemDBUrl);
+            if (upcItemDBResponse.IsSuccessStatusCode)
+            {
+                string asinInfo = await upcItemDBResponse.Content.ReadAsStringAsync();
+                var upcItemDBJson = JObject.Parse(asinInfo);
+                details.Asin = upcItemDBJson["items"]?[0]?["asin"]?.ToString() ?? null;
+                if (string.IsNullOrEmpty(details.Name))
                 {
-                    return await response.Content.ReadAsStringAsync();
+                    details.Name = upcItemDBJson["items"]?[0]?["title"]?.ToString();
                 }
-                else
+            }
+            // Fetch ingredients from another API
+            string bigProductDataUrl = $"https://big-product-data.p.rapidapi.com/gtin/{barcode}";
+            client.DefaultRequestHeaders.Clear();
+            client.DefaultRequestHeaders.Add("X-RapidAPI-Key", APIKEY);
+            client.DefaultRequestHeaders.Add("X-RapidAPI-Host", "big-product-data.p.rapidapi.com");
+            var bigProductDataResponse = await client.GetAsync(bigProductDataUrl);
+            if (bigProductDataResponse.IsSuccessStatusCode)
+            {
+                string bigProductDataInfo = await bigProductDataResponse.Content.ReadAsStringAsync();
+                var bigProductDataJson = JObject.Parse(bigProductDataInfo);
+                details.Ingredients = bigProductDataJson["properties"]?["ingredients"]?.ToString() ?? null;
+                if (string.IsNullOrEmpty(details.Name))
                 {
-                    throw new HttpRequestException($"Failed to retrieve product info. Status code: {response.StatusCode}");
+                    details.Name = bigProductDataJson["properties"]?["name"]?.ToString();
                 }
+            }
+            return details;
+        }
+
+        public static async Task<List<SellerOffer>> AmazonSearchResultASIN(string asin)
+        {
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri($"https://real-time-amazon-data.p.rapidapi.com/product-offers?asin={asin}&country=US&limit=10&page=1"),
+                Headers =
+        {
+            { "X-RapidAPI-Key", APIKEY },
+            { "X-RapidAPI-Host", "real-time-amazon-data.p.rapidapi.com" },
+        },
+            };
+
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var jsonResponse = JObject.Parse(body);
+
+                var offers = jsonResponse["data"]["product_offers"]
+                .Select(o => new SellerOffer
+                {
+                    ProductPrice = (string)o["product_price"],
+                    ProductCondition = (string)o["product_condition"],
+                    ShipsFrom = (string)o["ships_from"],
+                    Seller = (string)o["seller"],
+                    SellerLink = (string)o["seller_link"],
+                    SellerStarRating = (string)o["seller_star_rating"],
+                    SellerStarRatingInfo = (string)o["seller_star_rating_info"],
+                    Currency = (string)o["currency"],
+                    DeliveryPrice = (string)o["delivery_price"],
+                    DeliveryTime = (string)o["delivery_time"]
+                })
+                .ToList();
+                return offers;
             }
         }
 
-        private static async Task<List<Offer>> AmazonSearchResult(string searchTerm, string APIKEY)
+        private static async Task<List<Offer>> AmazonSearchResult(string searchTerm)
         {
             List<Offer> offersList = new List<Offer>();
             var request = new HttpRequestMessage
             {
                 Method = HttpMethod.Get,
-                RequestUri = new Uri($"https://real-time-amazon-data.p.rapidapi.com/search?query={searchTerm}&page=1&country=US&category_id=aps"),
+                RequestUri = new Uri($"https://real-time-amazon-data.p.rapidapi.com/search?query={searchTerm}&page=1&country=US&sort_by=RELEVANCE&category_id=aps"),
                 Headers =
                 {
                     { "X-RapidAPI-Key", APIKEY },
@@ -102,7 +221,6 @@ namespace SQL_DATABASE.Controllers
                         Link = (string)p["product_url"]
                     })
                     .OrderByDescending(offer => offer.ReviewRating)
-                    .ThenByDescending(offer => offer.ReviewCount)
                     .Take(3)
                     .ToList();
 
